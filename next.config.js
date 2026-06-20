9e4567f0350ce68b4059e5ae878bdd487d9a3c53
Dilam Polar <dilampolar@gmal.com>
2026-06-20 04:48:40 -0500

    Actualizar configuracion de Vercel

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', 
+}
+
+module.exports = nextConfig
